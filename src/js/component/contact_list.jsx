1854f5570d5955598ad4d1c1626cf70fb2d8a85c
Daniel Martinez <d.martinez74@gmail.com>
2023-04-07 20:02:03 +0000

    Contact list created

--- src/js/component/contact_list.jsx
+++ src/js/component/contact_list.jsx
@@ -23,10 +23,51 @@ export const ContactList = () => {
                         <i className="fa-solid fa-trash-can me-2"></i>
                     </div>
                 </li>
-                <li className="list-group-item">Dapibus ac facilisis in</li>
-                <li className="list-group-item">Morbi leo risus</li>
-                <li className="list-group-item">Porta ac consectetur ac</li>
-                <li className="list-group-item">Vestibulum at eros</li>
+                <li className="list-group-item d-flex">
+                    <div className="me-3">
+                        <img src={contact} alt="..." className="contact-image rounded-circle" />
+                    </div>
+                    <div className="d-flex flex-column p-2">
+                        <p className="name">Name</p>
+                        <p className="text-secondary fw-bold"><i className="fa-solid fa-location-dot "></i> Address</p>
+                        <p className="text-secondary"><i className="fa-solid fa-phone text-secondary"></i> Phone</p>
+                        <p className="text-secondary "><i className="fa-solid fa-envelope text-secondary"></i> Email</p>
+                    </div>
+                    <div className="icons-edit ms-auto">
+                        <i className="fa-solid fa-pencil me-4"></i>
+                        <i className="fa-solid fa-trash-can me-2"></i>
+                    </div>
+                </li>
+                <li className="list-group-item d-flex">
+                    <div className="me-3">
+                        <img src={contact} alt="..." className="contact-image rounded-circle" />
+                    </div>
+                    <div className="d-flex flex-column p-2">
+                        <p className="name">Name</p>
+                        <p className="text-secondary fw-bold"><i className="fa-solid fa-location-dot "></i> Address</p>
+                        <p className="text-secondary"><i className="fa-solid fa-phone text-secondary"></i> Phone</p>
+                        <p className="text-secondary "><i className="fa-solid fa-envelope text-secondary"></i> Email</p>
+                    </div>
+                    <div className="icons-edit ms-auto">
+                        <i className="fa-solid fa-pencil me-4"></i>
+                        <i className="fa-solid fa-trash-can me-2"></i>
+                    </div>
+                </li>
+                <li className="list-group-item d-flex">
+                    <div className="me-3">
+                        <img src={contact} alt="..." className="contact-image rounded-circle" />
+                    </div>
+                    <div className="d-flex flex-column p-2">
+                        <p className="name">Name</p>
+                        <p className="text-secondary fw-bold"><i className="fa-solid fa-location-dot "></i> Address</p>
+                        <p className="text-secondary"><i className="fa-solid fa-phone text-secondary"></i> Phone</p>
+                        <p className="text-secondary "><i className="fa-solid fa-envelope text-secondary"></i> Email</p>
+                    </div>
+                    <div className="icons-edit ms-auto">
+                        <i className="fa-solid fa-pencil me-4"></i>
+                        <i className="fa-solid fa-trash-can me-2"></i>
+                    </div>
+                </li>
             </ul>
         </div>
     );
