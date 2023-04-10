5ffc2bfe29d5067ce687c25e62f3439236066d10
Daniel Martinez <d.martinez74@gmail.com>
2023-04-10 15:08:32 +0000

    Contact card added

--- src/js/component/contact_card.jsx
+++ src/js/component/contact_card.jsx
@@ -1,26 +1,31 @@
 import React from "react";
 import { Button, Card, Modal } from 'react-bootstrap';
 import { ContactList } from "./contact_list.jsx";
+import contact from "../../img/contact.jpg";
 
-export const ContactCard = ({showCard, handleCloseCard}) => {
+export const ContactCard = ({ showCard, handleCloseCard }) => {
     return (
         <>
             <Modal show={showCard} onHide={handleCloseCard}>
-                  <Modal.Body>
+                <Modal.Body>
                     <Card show={showCard} onHide={handleCloseCard}>
-                        <Card.Img variant="top" src="holder.js/100px180" />
+                        <Card.Img variant="top" src={contact} className="contact-image rounded-circle" />
                         <Card.Body>
-                            <Card.Title>Card Title</Card.Title>
+                            <Card.Title>Contact Card</Card.Title>
                             <Card.Text>
-                                Some quick example text to build on the card title and make up the
-                                bulk of the card's content.
-                            </Card.Text>
-                            <Button variant="primary" onClick={handleCloseCard}>Go somewhere</Button>
-                        </Card.Body>
-                    </Card>
-                </Modal.Body>
-            </Modal>
-            {/* <Button variant="success" onClick={handleShowModal}>
+                            <div className="d-flex flex-column p-2">
+                                <p className="name">Name</p>
+                                <p className="text-secondary fw-bold"><i className="fa-solid fa-location-dot "></i> Address</p>
+                                <p className="text-secondary"><i className="fa-solid fa-phone text-secondary"></i> Phone</p>
+                                <p className="text-secondary "><i className="fa-solid fa-envelope text-secondary"></i> Email</p>
+                            </div>
+                        </Card.Text>
+                        <Button variant="secondary" onClick={handleCloseCard}>Close</Button>
+                    </Card.Body>
+                </Card>
+            </Modal.Body>
+        </Modal >
+        {/* <Button variant="success" onClick={handleShowModal}>
                 Add new contact
             </Button> */}
         </>
